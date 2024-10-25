d484ba92de24729e95b070ac8328b154267685df
Andre Pinto <andrepinto.cg@gmail.com>
2024-10-25 02:01:41 -0300

    add carroussel images

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-const menuIcon = document.getElementById('menu-icon');
-const navLinks = document.getElementById('nav-links');
+document.addEventListener('DOMContentLoaded', function() {
+    const imagens = [
+        'img/slider1.jpg',
+        'img/slider2.jpg',
+        'img/slider3.jpg'
 
-menuIcon.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+    ];
+
+    const imagem = document.querySelector('.banner img');
+    imagem.src = imagens[0];
+    setInterval(trocarImagem, 3000);
+
+    function trocarImagem(){
+        
+        const primeiro = imagens.shift(); // Remove a primeira imagem do array
+        imagens.push(primeiro); // Coloca a imagem no final do array
+
+        const novoPrimeiro = imagens[0];
+        imagem.src = novoPrimeiro;
+    }
+
+    const menuIcon = document.getElementById('menu-icon');
+    const navLinks = document.getElementById('nav-links');
+
+    menuIcon.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+
+    });
 });
